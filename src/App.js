177d9987b31ead5a97b3d7eb2e9e5a47a0900078
Mike Mendes <miekal2021@gmail.com>
2022-03-08 16:16:23 -0500

    Working on styling for body of site

--- src/App.js
+++ src/App.js
@@ -3,21 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{
+        backgroundColor: 'lightgrey',
+        width: '100px',
+        height: '100px'
+    }}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Micheal Mendes</h1>
+        <h2>Software Engineer, Recent Graduate of Worcester State University</h2>
       </header>
+      <p>
+        This is some text to be used for troubleshooting. Here are some more words.
+        And some more.
+      </p>
+      <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
 }
