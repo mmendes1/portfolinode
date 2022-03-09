06be6b202375adf3ac11a9cb985edab3e60286fa
Mike Mendes <miekal2021@gmail.com>
2022-03-09 16:27:49 -0500

    Working on adding images/gifs for header and projects sectionm

--- src/App.js
+++ src/App.js
@@ -4,18 +4,26 @@ import './App.css';
 function App() {
   return (
     <div className="App" style={{
-        backgroundColor: 'lightgrey',
-        width: '100px',
-        height: '100px'
-    }}>
+        backgroundImage: 'linear-gradient(to bottom right, rgb(44, 43, 43), rgb(95, 54, 104))',
+        width:'100%',
+        height:'100%'
+      }}>
       <header className="App-header">
         <h1>Micheal Mendes</h1>
         <h2>Software Engineer, Recent Graduate of Worcester State University</h2>
       </header>
-      <p>
-        This is some text to be used for troubleshooting. Here are some more words.
-        And some more.
+      <p className='personalDesc' style={{
+          height: '10%',
+          width: '100%'
+        }}>
+        I am a recent graduate of Worcester State University with a B.S. in Computer Science with a focus on Software Development and a minor in Mathematics.
+        I have a strong backgorund in Java but have exposure to a variety of languages, such as C# and Javascript. This is my personal site though, so no need
+        to bore you with my whole resume here, all that fun stuff can be seen on my LinkedIn profile in the footer.
       </p>
+      <div className='App-projects' style={{width: '100%', height: '10%'}}>
+        <h3>Projects</h3>
+        <img src='visuals/Background.jpg' class='Header-backghround' alt='Cityscape Background'></img>
+      </div>
       <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
