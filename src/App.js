a01ff7d0ec5ca931d67993b973c322e602441d6d
Mike Mendes <miekal2021@gmail.com>
2022-03-11 21:18:04 -0500

    Added images and dividers, working on styling for project section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import logo from './logo.svg';
+import Background from './visuals/Background.jpg';
+import Project1 from './visuals/mm_timetrial.gif';
 import './App.css';
 
 function App() {
@@ -8,23 +10,37 @@ function App() {
         width:'100%',
         height:'100%'
       }}>
-      <header className="App-header">
+      <header className="App-header" style={{
+        position: 'relative'
+      }}>
+      <img src={Background} className="Header-background" alt="Background"/>
         <h1>Micheal Mendes</h1>
         <h2>Software Engineer, Recent Graduate of Worcester State University</h2>
       </header>
+      <span class="line"></span>
       <p className='personalDesc' style={{
-          height: '10%',
-          width: '100%'
+          height: '100%',
+          width: '40%',
+          textAlign: 'center',
+          padding: '0% 30% 0%'
         }}>
         I am a recent graduate of Worcester State University with a B.S. in Computer Science with a focus on Software Development and a minor in Mathematics.
         I have a strong backgorund in Java but have exposure to a variety of languages, such as C# and Javascript. This is my personal site though, so no need
         to bore you with my whole resume here, all that fun stuff can be seen on my LinkedIn profile in the footer.
       </p>
+      <span className='divider'></span>
       <div className='App-projects' style={{width: '100%', height: '10%'}}>
         <h3>Projects</h3>
-        <img src='visuals/Background.jpg' class='Header-backghround' alt='Cityscape Background'></img>
-      </div>
-      <img src={logo} className="App-logo" alt="logo" />
+        <span class="line"></span>
+        <p style={{
+          margin: '0%',
+          padding: '1% 1% 1%'
+        }}>
+          Program to determine efficiency of different methods of matrix multiplication
+          </p>
+        <img src={Project1} className='Matrixproj-gif' alt='Project-1' />
+    </div>
+    <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
 }
